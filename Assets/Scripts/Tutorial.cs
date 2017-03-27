91a2594f897b381d0bd8a67c16ef10e92b309b37
Christopher Potter <potterc@wit.edu>
2017-03-27 11:32:44 -0400

    Tutorial Scene Started
    Started

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour {
+    public Sprite[] imageArray;
+    public int currentImage;
+    private GameObject imagePanel;
+
+    private void Start()
+    {
+        imagePanel = GameObject.FindGameObjectWithTag("ImagePanel");
+
+    }
+
+
+    public void onRightDown()
+    {
+        Debug.Log("RIGHT BUTTON PRESSED");
+        currentImage++;
+        updateImagePanel();
+    }
+
+    public void onLeftDown()
+    {
+        Debug.Log("LEFT BUTTON PRESSED");
+        currentImage--;
+        updateImagePanel();
+        
+    }
+
+    private void updateImagePanel()
+    {
+        SpriteRenderer sr = imagePanel.GetComponent<SpriteRenderer>();
+        sr.sprite = imageArray[currentImage];
+    }
+}
