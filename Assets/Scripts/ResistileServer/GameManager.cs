a3ec5d2c7f7c4571ce558a70ff2484b7eaa4e5ae
Rob <seymourr1@wit.edu>
2017-04-02 17:47:26 -0400

    GameManager started

--- Assets/Scripts/ResistileServer/GameManager.cs
+++ Assets/Scripts/ResistileServer/GameManager.cs
@@ -8,24 +8,65 @@ namespace ResistileServer
 {
     class GameManager
     {
+
         private const int MAXHAND = 5;
+        private const double primaryMIN = 8.0;
+        private const double primaryMAX = 17.0;
+        private const double secondaryMIN = 1;
+        private const double secondaryMAX = 6;
+        int[] secondaryObj = new int[2];
+        Random random = new Random();
+
         Player playerOne;
         Player playerTwo;
         //BoardManager board;
         DeckManager deck;
         ArrayList tempHand;
 
-
-
-        GameManager(String playerOneUsername, String playerTwoUsername, int clientID, int gameID)
+        GameManager(string playerOneUsername, string playerTwoUsername, int clientID, int gameID)
         {
             // board = new BoardManager;
             deck = new DeckManager();
-            for(int i=0; i<MAXHAND; i++)
+            //Initialize playerOne
+            for (int i = 0; i < MAXHAND; i++)
             {
                 tempHand.Add(deck.draw());
             }
-            //playerOne = new Player();
+            playerOne = new Player(playerOneUsername, tempHand, GetRandomPrimary(primaryMIN, primaryMAX), secondaryObj);
+            //Initialize playerTwo
+            for (int i = 0; i < MAXHAND; i++)
+            {
+                tempHand.Add(deck.draw());
+            }
+            playerTwo = new Player(playerTwoUsername, tempHand, GetRandomPrimary(primaryMIN, primaryMAX), secondaryObj);
+        }
+
+
+
+
+
+
+
+
+
+
+        private double GetRandomPrimary(double minimum, double maximum)
+        {
+            return random.NextDouble() * (maximum - minimum) + minimum;
+        }
+
+        private int GetRandomSecondary(double minimum, double maximum)
+        {
+            int randomNumber = random.Next(secondaryMIN, secondaryMAX);
+        }
+
+        private void CreateSecondaryObj()
+        {
+            secondaryObj[0] = GetRandomSecondary;
+            do
+            {
+                secondaryObj[1] = GetRandomSecondary;
+            } while (secondaryObj[0] == secondaryObj[1]);
         }
     }
 }
