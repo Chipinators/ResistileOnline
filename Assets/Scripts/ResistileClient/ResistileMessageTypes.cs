5095313979940a6c1ad159f30cc3e72915df2ef9
Christopher Potter <potterc@wit.edu>
2017-04-04 15:42:51 -0400

    Finalized ResistileMessageTypes.cs
    https://docs.google.com/spreadsheets/d/1MAvaZt6e1IPcIGNM1UAjtIxoKIZIAuIWq0x1jJ1KvVs/edit#gid=0

--- Assets/Scripts/ResistileClient/ResistileMessageTypes.cs
+++ Assets/Scripts/ResistileClient/ResistileMessageTypes.cs
@@ -7,45 +7,37 @@ namespace ResistileClient
 {
     class ResistileMessageTypes
     {
-        //Client to Server
-
-        public const int hostWaiting = 1;
-
-        public const int initializeGame = 7;
-        public const int validMoves = 8;
-        public const int playerTurn = 9;
-        public const int newTiles = 10;
-
-
-        //Server to Client
-        public const int initialize = 13;
-        public const int host = 14;
-        public const int cancelHost = 15;
-        public const int start = 16;
-        public const int serverList = 17;
-        public const int gameLoaded = 20;
-        public const int rotate = 21;
-        public const int endTurn = 22;
-        public const int resGuess = 23;
-
-
-        //CURRENTLY USED
+        //General
         public const int ping = 0;
-        public const int login = 25;
-        public const int opponentFound = 26;
-        public const int opponentCanceled = 27;
-        public const int tilePlaced = 28;
-        public const int draw = 29;
-        public const int gameOver = 30;
-        public const int replay = 12;
-        public const int gameResults = 11;
-        public const int hostList = 4;
-        public const int hostDecline = 5;
-        public const int startGame = 6;
-        public const int quitGame = 24;
-        public const int joinLobby = 18;
-        public const int cancelJoin = 19;
-
-
+        //Login Scene
+        public const int login = 1;
+        //Host Scene
+        public const int startHosting = 2;
+        public const int opponentFound = 3;
+        public const int opponentCanceled = 4;
+        public const int startGame = 5;
+        public const int cancelSearch = 6;
+        public const int declineOpponent = 7;
+        public const int acceptOpponent = 8;
+        //Server Browser
+        public const int getHostList = 6;
+        public const int hostList = 7;
+        public const int hostDeclined = 8;
+        public const int requestJoinGame = 9;
+        public const int cancelJoinRequest = 10;
+        //In Game
+        public const int initializeGame = 11;
+        public const int tilePlaced = 12;
+        public const int drawResistor = 13;
+        public const int drawWire = 14;
+        public const int invalidMove = 15;
+        public const int gameResults = 16;
+        public const int replay = 17;
+        public const int opponentQuit = 18;
+        public const int gameLoaded = 19;
+        public const int quitGame = 20;
+        public const int endTurn = 21;
+        public const int rotateTile = 22;
+        public const int guessResistance = 23;
     }
 }
