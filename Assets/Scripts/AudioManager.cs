331f84e301f4407a064d356a7fd91fc0025f8209
Christopher Potter <potterc@wit.edu>
2017-04-10 18:22:54 -0400

    Close #8, Implemented Settings, Implemented Background Music
    TODO: 
    Need to create new message type to tell server when user changes their name

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+    public static AudioManager audioManager;
+
+    void Start()
+    {
+        audioManager = this;
+    }
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+}
--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,8 +1,98 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Settings : MonoBehaviour {
+
+    public InputField usernameField;
+    public Slider volumeSlider;
+    public Dropdown resolutionDropdown;
+    public Resolution[] resList;
+    public Toggle fullscreenToggle;
+    private bool isFullscreen;
+    private bool nameChange = false, resChange = true, fullscreenChange = false;
+
+    void Start()
+    {
+        usernameField.transform.FindChild("Placeholder").GetComponent<Text>().text = NetworkManager.networkManager.username;
+        volumeSlider.GetComponent<Slider>().value = AudioManager.audioManager.GetComponent<AudioSource>().volume;
+        resolutionDropdown.interactable = !isFullscreen;
+        isFullscreen = Screen.fullScreen;
+        fillResolutions();
+        Resolution curRes = Screen.currentResolution;
+        if (!curRes.Equals(resList[0])) resChange = true;
+    }
+
+    public void ApplySettings()
+    {
+        if (nameChange) NetworkManager.networkManager.username = usernameField.transform.FindChild("Text").GetComponent<Text>().text;
+        if (resChange) setResolution();
+        if (fullscreenChange) setFullScreen();
+    }
+
+    public void setVolume(float value)
+    {
+        AudioManager.audioManager.GetComponent<AudioSource>().volume = volumeSlider.GetComponent<Slider>().value;
+    }
+
+    public void setResolution()
+    {
+        if (!isFullscreen)
+        {
+            resolutionDropdown.interactable = !isFullscreen;
+        }
+        Resolution setRes = resList[resolutionDropdown.value];
+        Screen.SetResolution(setRes.width, setRes.height, false);
+        
+    }
+
+    public void setFullScreen()
+    {
+        isFullscreen = !isFullscreen;
+        if (isFullscreen)
+        {
+            Resolution[] allResolutions = Screen.resolutions;
+            Resolution maxResolution = allResolutions[allResolutions.Length - 1];
+            Screen.SetResolution(maxResolution.width, maxResolution.height, true);
+        }
+        else
+        {
+            setResolution();
+        }
+    }
     
 
+    private void fillResolutions()
+    {
+        resList = new Resolution[4];
+        Resolution tmp = new Resolution();
+        tmp.width = 960;
+        tmp.height = 540;
+        resList[0] = tmp;
+        tmp.width = 1280;
+        tmp.height = 720;
+        resList[1] = tmp;
+        tmp.width = 1600;
+        tmp.height = 900;
+        resList[2] = tmp;
+        tmp.width = 1920;
+        tmp.height = 1080;
+        resList[3] = tmp;
+    }
+
+
+    public void nameChanged()
+    {
+        nameChange = true;
+    }
+    public void resolutionChanged()
+    {
+        resChange = true;
+    }
+    public void fullscreenChanged()
+    {
+        fullscreenChange = true;
+        resolutionDropdown.interactable = !isFullscreen;
+    }
 }
